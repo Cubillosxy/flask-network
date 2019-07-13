2ad2fbb9ccad8390bca69c3334e4cbbf949c6276
edwin <cubillos.edwin.ext@nokia.com>
2019-07-13 12:39:44 -0500

    :construction: :whale: db integration

--- Blog/flask_blog.py
+++ Blog/flask_blog.py
@@ -1,4 +1,6 @@
 from os.path import join
+from datetime import datetime
+import time
 
 from flask import Flask
 from flask import flash
@@ -12,7 +14,56 @@ from forms import RegistrationForm
 
 app = Flask(__name__)
 
+
+DBUSER = 'postgres'
+DBPASS = 'foobarbaz'
+DBHOST = 'db'
+DBPORT = '5432'
+DBNAME = 'postgres'
+
+
+#DBUSER = 'flask'
+#DBPASS = 'flaskpass'
+#DBNAME = 'flask_db'
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
+    user=DBUSER,
+    passwd=DBPASS,
+    host=DBHOST,
+    port=DBPORT,
+    db=DBNAME
+)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '95b54bbdcb40a3e4ef9fcc3de57ca763'
+
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default_img.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author',  lazy=True)
+
+    def __repr__(self):
+        return f'User: {self.username}, '
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f'POST {self.id}, {self.title}'
+
+
 
 mock_post = [
     {
@@ -68,5 +119,28 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 
+def database_initialization_sequence():
+    db.create_all()
+    #test_rec = User(
+     #   'edwinc',
+      #  'edwin@gmail.com',
+       # 'demo.jpg',
+       # 'demopass',
+    #)
+
+    #db.session.add(test_rec)
+    #db.session.rollback()
+    #db.session.commit()
+
+
 if __name__ == '__main__':
+    dbstatus = False
+    while dbstatus == False:
+        try:
+            db.create_all()
+        except:
+            time.sleep(2)
+        else:
+            dbstatus = True
+    database_initialization_sequence()
     app.run(debug=True, host='0.0.0.0')
